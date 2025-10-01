2b92b685ad8c948bf9f40d05dc74781c44178e74
ShorooqAbuZaid <abuzaidshorooq@gmail.com>
2025-10-01 11:56:06 +0300

    Task-1 is done

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,10 @@
+function StudentCard(){
+    return(
+        <div>
+            <h3>Name: YOUR_NAME</h3>
+            <p>ID: YOUR_STUDENT_ID</p>
+            <p>Department: YOUR_DEPARTMENT_NAME</p>
+        </div>
+    );
+}
+export default StudentCard;
